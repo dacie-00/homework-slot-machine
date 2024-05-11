bcede82518c2342e5fed056b536e97185049bcd5
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-05-11 18:34:16 +0300

    Implement win condition checking

--- slot-machine.php
+++ slot-machine.php
@@ -3,10 +3,38 @@
 require_once("helpers.php");
 
 $properties = [
-    "width" => 4,
+    "width" => 3,
     "height" => 3,
-    "winConditions" => []
+    "winConditions" => [[[0, 0], [0, 1], [0, 2], [1, 2]]]
 ];
+
+function checkMatch(stdClass $board, stdClass $element, array $condition, $x, $y): bool {
+    $matchSymbol = $board->content[$y][$x]->symbol;
+    foreach ($condition as $position) {
+        if (!isset($board->content[$y + $position[1]][$x + $position[0]]->symbol)) {
+            return false;
+        }
+        if ($matchSymbol != $board->content[$y + $position[1]][$x + $position[0]]->symbol) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function findMatches(stdClass $board, array $winConditions) {
+    $matches = [];
+    foreach ($winConditions as $condition) {
+        foreach ($board->content as $y => $row) {
+            foreach ($row as $x => $element) {
+                $matchFound = checkMatch($board, $element, $condition, $x, $y);
+                if ($matchFound) {
+                    $matches[] = [$element, $condition, $x, $y];
+                }
+            }
+        }
+    }
+    return $matches;
+}
 
 function fillBoard(stdClass $board) {
     for ($y = 0; $y < $board->height; $y++) {
@@ -16,6 +44,7 @@ function fillBoard(stdClass $board) {
         }
     }
 }
+
 
 function createElement($symbol, $weight, $value) {
     $element = new stdClass();
@@ -42,9 +71,9 @@ function createBoard($width, $height)
 
 function displayBoard(stdClass $board)
 {
-    foreach ($board->content as $row) {
-        foreach ($row as $value) {
-            echo $value->symbol;
+    foreach ($board->content as $y => $row) {
+        foreach ($row as $x => $element) {
+            echo $element->symbol;
         }
         echo "\n";
     }
@@ -54,3 +83,7 @@ function displayBoard(stdClass $board)
 $board = createBoard($properties["width"], $properties["height"]);
 fillBoard($board);
 displayBoard($board);
+$matches = findMatches($board, $properties["winConditions"]);
+foreach ($matches as $match) {
+    echo "{$match[0]->symbol}, $match[2], $match[3]\n";
+}
