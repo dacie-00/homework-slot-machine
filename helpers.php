f4042289a22c6a09a6de33dcb510aa931c712b29
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-05-11 18:33:58 +0300

    Fix weighted random function

--- helpers.php
+++ helpers.php
@@ -5,14 +5,28 @@ function formatCurrency(int $amount): string
     return '$' . number_format($amount / 100, 2);
 }
 
-function weightedRandom($elements) {
-    $total = array_sum(array_column($elements, "weight"));
-    $randomValue = rand(0, $total);
+//function weightedRandom($elements) {
+//    $total = array_sum(array_column($elements, "weight"));
+//    $randomValue = rand(0, $total);
+//    foreach ($elements as $element) {
+//        if ($randomValue <= $element->weight) {
+//            return $element;
+//        }
+//        $randomValue -= $element->weight;
+//    }
+//    throw new Exception("Weights not correctly defined!");
+//}
+function weightedRandom($elements)
+{
+    $randomValue = mt_rand(1, (int)array_sum(array_column($elements, "weight")));
+
     foreach ($elements as $element) {
-        if ($randomValue <= $element->weight) {
-            return $element;
+        if ($element->weight < 0) {
+            throw new InvalidArgumentException("Element weight cannot be negative");
         }
         $randomValue -= $element->weight;
+        if ($randomValue <= 0) {
+            return $element;
+        }
     }
-    throw new Exception("Weights not correctly defined!");
 }
